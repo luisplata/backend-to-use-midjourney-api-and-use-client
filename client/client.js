418cb6e5532a0d943a2b66ccf620f20b5c211b23
Luis Plata <www.luisplata@gmail.com>
2024-05-21 12:13:09 -0500

    New template and new format

--- client/client.js
+++ client/client.js
@@ -2,66 +2,77 @@
 const urlParams = new URLSearchParams(window.location.search);
 const password = urlParams.get('password');
 
-// Muestra el panel de error con el mensaje 'obteniendo key'
-const errorPanel = document.getElementById('errorPanel');
-
-// Cuando la página termine de cargar, envía la contraseña al servidor y obtén el token
-window.addEventListener('load', function () {
-    errorPanel.style.display = 'block';
-    errorPanel.textContent = 'Obteniendo key';
-    fetch('https://midjourney.peryloth.com/api/get-token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ password }),
-    })
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(err => {
-                    throw new Error(err.error);
-                });
-            }
-            return response.json();
+/**
+ * Preloader
+ */
+const preloader = document.querySelector('#preloader');
+var errorText = document.getElementById('message');
+var promptPanel = document.getElementById('prompt-fill');
+promptPanel.classList.add('visually-hidden');
+if (preloader) {
+    window.addEventListener('load', () => {
+        fetch('/api/get-token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ password }),
         })
-        .then(data => {
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(err => {
+                        throw new Error(err.error);
+                    });
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Guarda el token en localStorage
+                localStorage.setItem('apiToken', data.token);
+                console.log('Token guardado:', localStorage.getItem('apiToken'));
+                promptPanel.classList.remove('visually-hidden');
+                //Read from local storage and list the prompts
+                LoadDataFromStorage();
+                setTimeout(() => {
+                    preloader.classList.add('loaded');
+                }, 1000);
+                setTimeout(() => {
+                    preloader.remove();
+                }, 2000);
+            })
+            .catch((error) => {
+                localStorage.setItem('apiToken', null);
+                console.error('Error:', error);
+                errorText.textContent = `Error: ${error.message}`;
+                setTimeout(() => {
+                    preloader.classList.add('loaded');
+                }, 1000);
+                setTimeout(() => {
+                    preloader.remove();
+                }, 2000);
+            });
+    });
+}
 
-            // Guarda el token en localStorage
-            localStorage.setItem('apiToken', data.apiToken);
-
-            // Oculta el panel de error y el panel de carga
-            errorPanel.style.display = 'none';
-            const loadingPanel = document.getElementById('loadingPanel');
-            loadingPanel.style.display = 'none';
-
-            // Muestra el contenido de la página
-            const content = document.getElementById('content');
-            content.style.display = 'block';
-        })
-        .catch((error) => {
-            console.error('Error two:', error);
-            console.error('Error:', error);
-            errorPanel.textContent = 'Error: ' + error.message;
-            errorPanel.style.display = 'block';
-        });
-});
 
 document.getElementById('sendPrompt').addEventListener('click', function () {
-    const promptInput = document.getElementById('promptInput');
+    const promptInput = document.getElementById('prompt');
     const prompt = promptInput.value;
-
-    // Muestra el panel de carga
-    const loadingPanel = document.getElementById('loadingPanel');
-    loadingPanel.style.display = 'block';
+    promptPanel.classList.add('visually-hidden');
+    errorText.textContent = `Loading! Please wait...`;
 
     // Obtén el token de localStorage
     const apiToken = localStorage.getItem('apiToken');
 
-    fetch('https://midjourney.peryloth.com/api/imagine', {
+    if (!apiToken || apiToken === 'null' || apiToken === 'undefined' || apiToken === '' || apiToken === null || apiToken === undefined) {
+        errorText.textContent = 'Error: No token';
+        return;
+    }
+
+    fetch('/api/imagine', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            // Incluye el token en la cabecera Authorization
             'Authorization': `Bearer ${apiToken}`,
         },
         body: JSON.stringify({ prompt }),
@@ -73,27 +84,103 @@ document.getElementById('sendPrompt').addEventListener('click', function () {
             return response.json();
         })
         .then(data => {
-            const output = document.getElementById('output');
-            output.innerHTML += `<p>${data.message}: ${JSON.stringify(data.result)}</p>`;
-            if (data.upscale) {
-                output.innerHTML += `<p>Upscale: ${JSON.stringify(data.upscale)}</p>`;
+            /*
+            {
+                "message": "Imagine",
+                "result": "https://media.discordapp.net/attachments/1241165530090700931/1242502457041359049/servipublicitarios_37759_A_young_culinary_student_cooking_Chine_b6f27ebd-fcd1-4c47-8f3b-7fe51ab5bfe5.png?ex=664e1216&is=664cc096&hm=9d326a72e95e399cbacb15a8ad65d876e04238896dffd079ecbf548822f27d41&",
+                "upscale": [
+                    "https://media.discordapp.net/attachments/1241165530090700931/1242502464645763214/servipublicitarios_37759_A_young_culinary_student_cooking_Chine_bf83f136-585f-4cfd-888e-a2820ac6cf94.png?ex=664e1218&is=664cc098&hm=097f3c61ee14071003fc781db653904c73ff5e6bc243e3fcd67baca30fd0d62f&",
+                    "https://media.discordapp.net/attachments/1241165530090700931/1242502470983487619/servipublicitarios_37759_A_young_culinary_student_cooking_Chine_9bde0db0-67ba-42dd-8756-395d2fcb3581.png?ex=664e1219&is=664cc099&hm=65beca7936a1c58670f8c8056a2ae3d5ce969e42ed3e732eba71b283f18e0436&",
+                    "https://media.discordapp.net/attachments/1241165530090700931/1242502476582621304/servipublicitarios_37759_A_young_culinary_student_cooking_Chine_9c7e6e5e-bbfd-4743-b96f-76008a91b48e.png?ex=664e121a&is=664cc09a&hm=1969c3953ccc39b732a52d336571562073a7a9128af513b44590ed5080c1d86f&",
+                    "https://media.discordapp.net/attachments/1241165530090700931/1242502485726462093/servipublicitarios_37759_A_young_culinary_student_cooking_Chine_ad783575-56ec-47ff-b1db-5f6a1ed12e4a.png?ex=664e121d&is=664cc09d&hm=63a2755e8166eb5b190f8df034dd1c025b45ef3774f17f3731a7c88a5f988958&"
+                ],
+                "prompt": "A young culinary student cooking Chinese fried rice, the photo is taken from behind a shelf looking at the student as he shakes the bowl of rice over the flame, he is smiling at the camera, vibrant colors highlighting the steam rising from the wok, stainless steel kitchen equipment reflecting light, bustling atmosphere of a professional kitchen with other chefs in the background, the scene exudes excitement and passion for cooking, Photography, captured with a Canon EOS R5 and a 35mm f/1.8 lens, --ar 16:9 --v 5"
             }
-
-            // Assuming the image URL is in data.result.image
-            if (data.upscale) {
-                const resultImage = document.getElementById('resultImage');
-                resultImage.src = data.upscale;
-            }
-
-            loadingPanel.style.display = 'none';
+            */
+            data.upscale.forEach((element, index) => {
+                SaveDataToStorage({
+                    "result": data.result,
+                    "upscale": element,
+                    "prompt": data.prompt,
+                    "upscaler": "U" + (index + 1),
+                    "timestamp": new Date().toISOString(),
+                });
+            });
+            promptInput.value = '';
+            promptPanel.classList.remove('visually-hidden');
+            errorText.textContent = `Prompt saved! write a new one...`;
         })
         .catch((error) => {
             console.error('Error:', error);
-
-            // Muestra el panel de error
-            errorPanel.style.display = 'block';
-            errorPanel.textContent = 'Error: ' + error.message;
-
-            loadingPanel.style.display = 'none';
+            errorText.textContent = 'Error: No token';
         });
 });
+
+function LoadDataFromStorage() {
+    const promptList = document.getElementById('content-prompts');
+    //clean the prompt list
+    promptList.innerHTML = '';
+    const prompt = localStorage.getItem('prompts');
+    if (prompt) {
+        const prompts = JSON.parse(prompt);
+        //sort the prompts by timestamp the most recent first
+        console.log("before",prompts);
+        prompts.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+        console.log("after",prompts);
+        prompts.forEach((prompt, index) => {
+            promptList.innerHTML += createHtmlFromJson(prompt);
+        });
+    }
+    const glightbox = GLightbox({
+        selector: '.glightbox'
+    });
+}
+
+// whent the user click the button save prompt, save it in the local storage and list it in the prompt list
+function createHtmlFromJson(json) {
+    return `
+            <div class="col-xl-3 col-lg-4 col-md-6">
+                <div class="gallery-item h-100">
+                    <img src="${json.upscale}" class="img-fluid" alt="">
+                    <div class="gallery-links d-flex align-items-center justify-content-center">
+                        <a href="${json.upscale}"
+                            title="${json.upscaler} ${json.prompt}"
+                            class="glightbox preview-link">
+                            <i class="bi bi-arrows-angle-expand"></i>
+                        </a>
+                        <button class='btn btn-primary' onclick='copyToClipboard(JSON.stringify(${JSON.stringify(json)}))'>Copy</button>
+                    </div>
+                </div>
+            </div>
+    `;
+}
+
+function copyToClipboard(text) {
+    var textarea = document.createElement("textarea");
+    textarea.textContent = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    try {
+        return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+    } catch (ex) {
+        console.warn("Copy to clipboard failed.", ex);
+        return false;
+    } finally {
+        document.body.removeChild(textarea);
+    }
+}
+
+function SaveDataToStorage(data) {
+    const prompt = localStorage.getItem('prompts');
+    if (prompt) {
+        const prompts = JSON.parse(prompt);
+        prompts.push(data);
+        localStorage.setItem('prompts', JSON.stringify(prompts));
+    }
+    else {
+        const prompts = [];
+        prompts.push(data);
+        localStorage.setItem('prompts', JSON.stringify(prompts));
+    }
+    LoadDataFromStorage();
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { Midjourney } = require('midjourney');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const winston = require('winston');
 require('dotenv/config');
 app.use(express.json());
 app.use(cors());
@@ -16,9 +17,26 @@ const myPlaintextPassword = process.env.JWT_PASS;
 const saltRounds = 10;
 
 let hashedPassword = '';
-bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
     hashedPassword = hash;
-  });
+});
+
+//logs
+const generalLogger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'general.log' })
+    ]
+});
+
+const specificLogger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'specific.log' })
+    ]
+});
 
 const client = new Midjourney({
     ServerId: process.env.SERVER_ID,
@@ -29,13 +47,14 @@ const client = new Midjourney({
 });
 
 app.get('/', (req, res) => {
+    generalLogger.info('GET /');
     res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
 function verifyToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-
+    const uri = req.originalUrl;
     if (token == null) {
         return res.sendStatus(401);
     }
@@ -43,7 +62,9 @@ function verifyToken(req, res, next) {
         if (err) {
             return res.sendStatus(403);
         }
+        user.token = token;
         req.user = user;
+        specificLogger.info(`verifyToken: <${token}> user ${JSON.stringify(user)} wants to access the app at ${uri}`);
         next();
     });
 }
@@ -56,43 +77,54 @@ app.post('/api/info', verifyToken, async (req, res) => {
 
 app.post('/api/imagine', verifyToken, async (req, res) => {
     await client.init();
-    const prompt = req.body.prompt;
-    const Imagine = await client.Imagine(
-        prompt,
-        (uri, progress) => {
-            console.log("loading", uri, "progress", progress);
+    try {
+        let promptOriginal = req.body.prompt;
+        let prompt = encodeURIComponent(promptOriginal);
+        specificLogger.info(`/api/imagine: <${req.user.token}> New prompt ${prompt}`);
+        const Imagine = await client.Imagine(
+            prompt,
+            (uri, progress) => {
+                generalLogger.info("loading", uri, "progress", progress);
+            }
+        );
+        generalLogger.info(Imagine);
+        specificLogger.info(`/api/imagine: <${req.user.token}> New Imagine ${Imagine.proxy_url}`);
+        if (!Imagine) {
+            generalLogger.info("no message");
+            return res.json({ message: 'No message' });
         }
-    );
-    console.log(Imagine);
-    if (!Imagine) {
-        console.log("no message");
-        return res.json({ message: 'No message' });
-    }
-    upscales = [];
-    for (let i = 1; i <= 4; i++) {
-        const label = `U${i}`;
-        const customID = Imagine.options?.find((o) => o.label === label)?.custom;
-        if (!customID) {
-            upscales.push({ message: `No ${label}` });
-            continue;
+        upscales = [];
+        for (let i = 1; i <= 4; i++) {
+            const label = `U${i}`;
+            const customID = Imagine.options?.find((o) => o.label === label)?.custom;
+            if (!customID) {
+                upscales.push({ message: `No ${label}` });
+                continue;
+            }
+            const Upscale = await client.Custom({
+                msgId: Imagine.id,
+                flags: Imagine.flags,
+                customId: customID,
+                loading: (uri, progress) => {
+                    generalLogger.info("loading", uri, "progress", progress);
+                },
+            });
+            if (!Upscale) {
+                generalLogger.info("no Upscale");
+                upscales.push({ message: 'No Upscale' });
+                continue;
+            }
+            specificLogger.info(`/api/imagine Upscale: <${req.user.token}> New Upscale ${Upscale.proxy_url}`);
+            upscales.push(Upscale.proxy_url);
         }
-        const Upscale = await client.Custom({
-            msgId: Imagine.id,
-            flags: Imagine.flags,
-            customId: customID,
-            loading: (uri, progress) => {
-                console.log("loading", uri, "progress", progress);
-            },
-        });
-        if (!Upscale) {
-            console.log("no Upscale");
-            upscales.push({ message: 'No Upscale' });
-            continue;
-        }
-        upscales.push(Upscale.proxy_url);
-    }
+        client.Close();
+        res.json({ message: 'Imagine', result: Imagine.proxy_url, upscale: upscales, prompt: promptOriginal });
 
-    res.json({ message: 'Imagine', result: Imagine.proxy_url, upscale: upscales });
+    } catch (error) {
+        client.Close();
+        generalLogger.error(error.message);
+        res.status(500).json({ error: error.message });
+    }
 });
 
 app.post('/api/get-token', async (req, res) => {
@@ -102,16 +134,17 @@ app.post('/api/get-token', async (req, res) => {
 
         if (match) {
             const apiToken = jwt.sign({ user: 'user' }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            specificLogger.info(`/api/get-token: New user connected ${apiToken}`);
             res.json({ token: apiToken });
         } else {
             res.status(403).json({ error: 'Invalid password; use `?password=[password]` to can use the app' });
         }
     } catch (error) {
-        console.error(error);
+        generalLogger.error(error);
         res.status(403).json({ error: 'Invalid password; use `?password=[password]` to can use the app' });
     }
 });
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+    generalLogger.info(`Servidor corriendo en http://localhost:${port}`);
 });
