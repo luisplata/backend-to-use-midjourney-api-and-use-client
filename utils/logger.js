33d15c6b60d7d6b48823e13024ad5bafef408c37
Luis Plata <www.luisplata@gmail.com>
2024-07-09 21:34:58 -0500

    logger imrpuve and fixed errores in client

--- utils/logger.js
+++ utils/logger.js
@@ -2,22 +2,73 @@ const winston = require('winston');
 
 const generalLogger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
     transports: [
-        new winston.transports.File({ filename: 'general.log' })
+        new winston.transports.File({ 
+            filename: 'general.log',
+            maxsize: 5242880, // 5MB
+            maxFiles: 5
+        }),
+        new winston.transports.Console()
     ]
 });
 
 const specificLogger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
     transports: [
-        new winston.transports.File({ filename: 'specific.log' })
+        new winston.transports.File({ 
+            filename: 'specific.log',
+            maxsize: 5242880, // 5MB
+            maxFiles: 5
+        }),
+        new winston.transports.Console()
     ]
 });
 
 function configureLogging() {
     // Additional logging configuration if necessary
 }
+// Función para sobrescribir console.log
+function overrideConsoleLog() {
+    const originalConsoleLog = console.log;
+
+    console.log = (...args) => {
+        // Convertir argumentos a string para el log
+        const message = args.map(arg => typeof arg === 'object' ? JSON.stringify(arg) : arg).join(' ');
+
+        // Loguear usando Winston
+        specificLogger.info(message);
+
+        // Opcional: Llamar al console.log original si aún quieres que los mensajes se muestren en la consola
+        // originalConsoleLog(...args);
+    };
+}
+
+// Sobrescribir console.error
+function overrideConsoleError() {
+    const originalConsoleError = console.error;
+
+    console.error = (...args) => {
+        // Convertir argumentos a string para el log
+        const message = args.map(arg => typeof arg === 'object' ? JSON.stringify(arg) : arg).join(' ');
+
+        // Loguear usando Winston
+        specificLogger.error(message);
+
+        // Opcional: Llamar al console.error original si aún quieres que los mensajes se muestren en la consola
+        // originalConsoleError(...args);
+    };
+}
+
+// Llamar a las funciones para sobrescribir
+overrideConsoleLog();
+overrideConsoleError();
+
 
 module.exports = { generalLogger, specificLogger, configureLogging };
