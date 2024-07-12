2b858fe25519538821780d5b7f7de818f28194fe
Luis Plata <www.luisplata@gmail.com>
2024-07-11 20:45:53 -0500

    create a funtion with prompt to we can read from outsite of the proyect

--- routes/api.js
+++ routes/api.js
@@ -83,7 +83,7 @@ router.post('/poli', verifyToken, async (req, res) => {
         const url = await sendPictureToDiscord(process.env.CHANNEL_ID_FACE, image);
         // The description functionality is commented out, so no changes are made here.
         const parameters = " --ar 3:4";
-        const prompt = `${url} ${context} ${parameters}`;
+        const prompt = `${url} ${GetPromptFromID(context)} ${parameters}`;
 
         await client.init();
         const Imagine = await client.Imagine(prompt, (uri, progress) => {
@@ -120,5 +120,39 @@ router.post('/poli', verifyToken, async (req, res) => {
         client.Close();
     }
 });
+
+function GetPromptFromID(id) {
+    console.log(`ID: ${id}`);
+    switch (id) {
+        case "Grupo1":
+            return "a partir de  la imagen manteniendo su apariencia realista para crear profesional de administración de empresas de estilo lujoso, en una oficina elegante, Incorpora con elementos de empresa gastronomica y hotelera";
+        case "Grupo2":
+            return "Convert the image into a detailed and precise financial illustration incorporate numbers and Financial items.";
+        case "Grupo3":
+            return "Convert the image into a naturalism style incorporate agronomy trade and ecology items";
+        case "Grupo4":
+            return "Create an artistic collage using the image and other visuals to incorporate Logistics and international Business items";
+        case "Grupo5":
+            return "Convert image into professional Workplace Safety and Human Resources drawing with dynamic perspective and depth effects";
+        case "Grupo6":
+            return "Turn the image into a professional lawyer in opulent style incorporate justness items";
+        case "Grupo7":
+            return "Convert the image to a teacher in classroom";
+        case "Grupo8":
+            return "turn the image into a psychologist in an office Transitional style digital illustration with sharp lines";
+        case "Grupo9":
+            return "Convert the image into a photograph with smooth light and motion effects incorporate journalism and mass media items";
+        case "Grupo10":
+            return "Create an artistic collage using the image and other visuals to incorporate marketing and publicity items postmodernism style";
+        case "Grupo11":
+            return "Give the image an industrial design painting look with precise details and clean lines to incorporate industrial design elements around";
+        case "Grupo12":
+            return "Give the image a futuristic look with technological and abstract elements";
+        case "Grupo13":
+            return "Give the image a stylish and sophisticated fashion illustration look";
+        case "Grupo14":
+            return "Convert the image into a detailed and precise financial an mathematical illustration+to incorporate numbers and mathematical items";
+    }
+}
 
 module.exports = router;
